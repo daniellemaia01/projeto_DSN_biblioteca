05b40fdbd17f960d3e8c6f4714a86bb44425b0b1
Danielle Maia <daniellemaia01@gmail.com>
2024-09-13 11:28:51 -0300

    Finzalizando a remoção Dvd

--- javascript/dvds.js
+++ javascript/dvds.js
@@ -1,5 +1,11 @@
 const BASE_URL_DVDS ='https://parseapi.back4app.com/classes/Dvds';
 
+// Para obter um parametro passado via GET na URL
+function getURLParamValue(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
 //Para listar todos os Dvds
 function listarDvds() {
 
@@ -52,7 +58,8 @@ function removerDvd(id) {
     });
     instanciaAxios.delete('/'+id).then(
         function (response) {
-            alert('Sucesso na remoção.')
+            alert('Sucesso na remoção do dvd.');
+            location.reload();
         }
     )
     .catch(function (error) {
@@ -60,4 +67,51 @@ function removerDvd(id) {
         alert('Erro ao listar dvds: ' + error.message);
         return;
     });
+}
+
+function editarDvd(){
+
+    let idDvd= getURLParamValue('id');
+
+    // Se está editando
+    if(idDvd){ 
+        let instanciaAxios= axios.create({
+            baseURL: BASE_URL_DVDS,
+            headers: {
+                'X-Parse-Application-Id': 'zQ8GBmAbVEIGSPmbMMKKYZHPiN3stv2Ou9ZyGoqN',
+                'X-Parse-REST-API-Key': '08h3KTkweA8TpKcRAyn5x9kPbM3hPBKjbwM6IXIA'
+            }
+        });
+        instanciaAxios.get().then(
+            function (response) {
+                // Na obtencao de dvds
+                const dvds = response.data.results;
+    
+                if (dvds.length == 0) {
+                    alert('Nenhum dvd localizado');
+                    return;
+                }
+    
+                // Popule a tabela com os dados dos dvds
+                const tabelaResultadosDvds = document.getElementById('tabela-dvds');
+                dvds.forEach(dvd => {
+                    const row = tabelaResultadosDvds.insertRow();
+                    row.innerHTML = `
+                    <td>${dvd.titulo}</td>
+                    <td>${dvd.ano}</td>
+                    <td>${dvd.genero}</td>
+                    <td><a href="edita_dvds.html?id=${dvd.objectId}" class="btn btn-primary">Editar</a>
+                        <a href="javascript:void(0)" onclick="javascript:removerDvd('${dvd.objectId}')"  class="btn btn-primary">Apagar</a>
+                    </td>`;
+                });
+    
+            }
+        )
+        .catch(function (error) {
+            // No caso de erro, apresentar na tela
+            alert('Erro ao listar dvds: ' + error.message);
+            return;
+        });
+
+    }
 }
